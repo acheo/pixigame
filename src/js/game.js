26671e31b1f20165e4c5743d15dabc97b9ec5079
Adam Cheong <adam@cheong.co>
2017-01-04 09:34:17 +0000

    Handle mouse events. Move ship to cursor.

--- src/js/game.js
+++ src/js/game.js
@@ -21,6 +21,20 @@ class Game {
         document.body.appendChild(this.renderer.view);
         let root = this.root = new PIXI.Container();
 
+        root.hitArea = new PIXI.Rectangle(0, 0, window.innerWidth, window.innerHeight);
+
+        var mouseMoveEventHandler = function(e){
+            //console.log(e);
+            game.myship.move(e.data.global.x,e.data.global.y);
+        }
+
+        root.interactive = true;
+        root.buttonMode = true;
+        //root.on("mousedown", mouseEventHandler);
+        //root.on("mouseup", mouseUpEventHandler);
+        //root.on("mouseover", mouseEventHandler);
+        root.on("mousemove", mouseMoveEventHandler);
+
         this.myship = new Ship( this, window.innerWidth / 2, window.innerHeight / 2);
 
     }
